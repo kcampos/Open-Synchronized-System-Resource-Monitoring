7cc721b34587657275adb0a8a9aed287413b9cf3
Kyle Campos <kyle.campos@gmail.com>
2011-10-25 17:58:19 -0700

    graphing support for net stat file parsing
    Using Gruff Charts, new gem required which depends on image magic

--- parse_stat_logs.rb
+++ parse_stat_logs.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
 
 require 'optparse'
+require 'rubygems'
+require 'gruff'
 
 
 # Initialize
 options = {}
 options[:debug] = false
+options[:graph] = false
 config = {}
 errors = [] # Gather arg validation errors
 
@@ -44,6 +47,11 @@ optparse = OptionParser.new do |opts|
     options[:db_file] = file
     errors.push("#{file} does not exist") unless(File.file?(options[:db_file]))
   end
+  
+  # Graph
+  opts.on('-g', '--graph', 'generate graphs') do
+    options[:graph] = true
+  end
     
 end
 
@@ -55,11 +63,12 @@ optparse.parse!
 # Net stat data
 class NetStat
   
-  attr_reader :values, :max, :min, :average
+  attr_reader :values, :max, :min, :average, :file, :col_data
   
   def initialize(file)
-    connection_str = `grep 'ESTABLISHED' #{file} | awk '{print $2}'`
-    @values = connection_str.split.collect{ |x| x.to_i }.sort
+    @file = file
+    @col_data = parse_net_stat_file
+    @values = parse_values
   end
   
   def max
@@ -80,11 +89,93 @@ class NetStat
     @values[(@values.size / 2)]
   end
   
+  def graph(opts={})
+    
+    defaults = {
+      :num_x_labels => 5,
+      :graph_filename => "#{self.file}.png"
+    }
+    
+    opts = defaults.merge(opts)
+    
+    # Build label hash, always want first and last
+    label_interval = @values.size/opts[:num_x_labels]
+    label_index = 0
+    labels = {}
+    for i in 1..opts[:num_x_labels] do
+      labels[label_index] = @col_data[label_index+1].keys[0].sub(/\s(AM|PM)/, '\1') #strip whitespace to conserve room
+      label_index += label_interval
+    end
+    
+    # Last entry
+    label_index = @col_data.keys.sort.pop - 1
+    labels[label_index] = @col_data[label_index].keys[0].sub(/\s(AM|PM)/, '\1')
+    
+    graph = Gruff::Line.new
+    graph.title = "Net Connection Statistics"
+    graph.labels = labels
+    graph.data 'Connections', @values
+    graph.write opts[:graph_filename]
+    
+    puts "Wrote graph to: #{opts[:graph_filename]}"
+  end
+  
   def to_s
     puts "Net Statistics"
     puts "---------------"
     puts "Min: [#{self.min}]\tMax: [#{self.max}]\tAvg: [#{self.average}]\tMed: [#{self.median}]"
     puts "------------------------------------------------------------"
+  end
+  
+  
+  private
+  
+  def parse_net_stat_file
+    
+    fh = File.open(self.file)
+    data = {}
+    interval = 0
+    
+    # Iterate over file
+    while(line = fh.gets)
+      
+      # Find start of section - timestamp
+      if(line =~ /^(\d{2}:\d{2}:\d{2}.+)/)
+        timestamp = $1
+        interval += 1
+        data[interval] = {}
+                
+        # Ingest all the data for this section
+        any_established = false
+        until(cur_line = fh.gets and cur_line =~ /^-/)
+          
+          if(cur_line =~ /ESTABLISHED: (\d+)/)
+            any_established = true
+            # Captured established connections
+            connections = $1.to_i
+            data[interval][timestamp] = connections              
+          end
+          
+        end
+        
+        # Since netstat shows nothing for 0 established connections, we'll fill it in here
+        data[interval][timestamp] = 0 if(!any_established)
+        
+      end
+      
+    end
+    
+    return data
+    
+  end
+  
+  def parse_values
+    values = []
+    @col_data.each_key do |interval|
+      @col_data[interval].each_value { |value| values << value}
+    end
+    
+    return values
   end
   
 end
@@ -268,6 +359,7 @@ end
 if(options[:net_file])
   net = NetStat.new(options[:net_file])
   net.to_s
+  net.graph if(options[:graph])
 end
 
 if(options[:db_file])
